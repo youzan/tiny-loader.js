28e266f351a8f201fc2c9409d4b562383a560f6c
pangxie <pangxie@qima-inc.com>
2015-11-18 16:39:30 +0800

    loader 修复老版本 webkit不支持link load事件

--- loader.js
+++ loader.js
@@ -1,39 +1,42 @@
 /*
-* #####Loader代码加载器
-* 用于脚本，样式的延迟加载、样式的预加载等
-* 脚本的延迟加载onLoad之后
-* **DEMO & API**
-*
-* // 脚本异步下载执行
-*  `window.Loader.async(['xxxx.js', 'yyyy.js'])`
-*
-* // 脚本同时下载，顺序执行
-*  `window.Loader.sync(['xxxx.js', 'yyyy.js'])`
-*
-* ```
-*   window.Loader.async(['xxxx.js'], function(){
-*        // 回调函数
-*    });
-* ```
-*/
+ * #####Loader代码加载器
+ * 用于脚本，样式的延迟加载、样式的预加载等
+ * 脚本的延迟加载onLoad之后
+ * **DEMO & API**
+ *
+ * // 脚本异步下载执行
+ *  `window.Loader.async(['xxxx.js', 'yyyy.js'])`
+ *
+ * // 脚本同时下载，顺序执行
+ *  `window.Loader.sync(['xxxx.js', 'yyyy.js'])`
+ *
+ * ```
+ *   window.Loader.async(['xxxx.js'], function(){
+ *        // 回调函数
+ *    });
+ * ```
+ */
 'use strict';
 
-(function(window, document){
+(function(window, document) {
     // cssExpr 用于判断资源是否是css
     var cssExpr = new RegExp('\\.css');
     var nHead = document.head || document.getElementsByTagName('head')[0];
+    // `onload` 在WebKit < 535.23， Firefox < 9.0 不被支持
+    var isOldWebKit = +navigator.userAgent
+        .replace(/.*(?:AppleWebKit|AndroidWebKit)\/?(\d+).*/i, '$1') < 536;
 
     // 判断对应的node节点是否已经载入完成
-    function isReady(node){
+    function isReady(node) {
         return node.readyState === 'complete' || node.readyState === 'loaded';
     }
 
     // loadCss 用于载入css资源
-    function loadCss(url, setting, callback){
+    function loadCss(url, setting, callback) {
         var node = document.createElement('link');
 
         node.rel = 'stylesheet';
-        addOnload(node, callback);
+        addOnload(node, callback, 'css');
         node.async = true;
         node.href = url;
 
@@ -41,31 +44,58 @@
     }
 
     // loadJs 用于载入js资源
-    function loadJs(url, setting, callback){
+    function loadJs(url, setting, callback) {
         var node = document.createElement('script');
 
         node.charset = 'utf-8';
-        addOnload(node, callback);
+        addOnload(node, callback, 'js');
         node.async = !setting.sync;
         node.src = url;
 
         nHead.appendChild(node);
     }
 
+    // 在老的webkit中，因不支持load事件，这里用轮询sheet来保证
+    function pollCss(node, callback){
+        var isLoaded;
+
+        if( node.sheet ){
+            isLoaded = true;
+        }
+
+        setTimeout(function() {
+            if (isLoaded) {
+                // 在这里callback 是为了让样式有足够的时间渲染
+                callback();
+            } else {
+                pollCss(node, callback);
+            }
+        }, 20);
+    }
+
     // 用于给指定的节点绑定onload回调
     // 监听元素载入完成事件
-    function addOnload(node, callback){
+    function addOnload(node, callback, type) {
         var supportOnload = 'onload' in node;
+        var isCSS = type === 'css';
+
+        // 对老的webkit和老的firefox的兼容
+        if (isCSS && (isOldWebKit || !supportOnload)) {
+            setTimeout(function() {
+                pollCss(node, callback);
+            }, 1);
+            return;
+        }
 
         if (supportOnload) {
             node.onload = onload;
-            node.onerror = function(){
+            node.onerror = function() {
                 node.onerror = null;
                 window._cdnFallback(node);
             };
         } else {
             node.onreadystatechange = function() {
-                if ( isReady(node) ) {
+                if (isReady(node)) {
                     onload();
                 }
             };
@@ -84,7 +114,7 @@
 
     // 资源下载入口，根绝文件类型的不同，调用loadCss或者loadJs
     function loadItem(url, list, setting, callback) {
-        if( cssExpr.test(url) ){
+        if (cssExpr.test(url)) {
             loadCss(url, setting, onFinishLoading);
         } else {
             loadJs(url, setting, onFinishLoading);
@@ -92,31 +122,31 @@
 
         // 每次资源下载完成后，检验是否结束整个list下载过程
         // 若已经完成所有下载，执行回调函数
-        function onFinishLoading(){
+        function onFinishLoading() {
             var urlIndex = list.indexOf(url);
-            if( urlIndex > -1 ){
+            if (urlIndex > -1) {
                 list.splice(urlIndex, 1);
             }
 
-            if( list.length === 0 ){
+            if (list.length === 0) {
                 callback();
             }
         }
     }
 
-    function doInit(list, setting, callback){
-        var cb = function(){
+    function doInit(list, setting, callback) {
+        var cb = function() {
             callback && callback();
         };
 
         list = Array.prototype.slice.call(list || []);
 
-        if( list.length === 0 ){
+        if (list.length === 0) {
             cb();
             return;
         }
 
-        for( var i = 0, len = list.length; i < len; i++ ){
+        for (var i = 0, len = list.length; i < len; i++) {
             loadItem(list[i], list, setting, cb);
         }
     }
@@ -125,7 +155,7 @@
     // 加载完，立刻执行下载
     // 未加载完，等待页面load事件以后再进行下载
     function ready(node, callback) {
-        if ( isReady(node) ) {
+        if (isReady(node)) {
             callback();
         } else {
             window.addEventListener('load', callback);
@@ -137,16 +167,16 @@
     // async 用作异步下载执行用，不阻塞页面渲染
     // sync  用作异步下载，顺序执行，保证下载的js按照数组顺序执行
     var Loader = {
-        async: function(list, callback){
+        async: function(list, callback) {
 
-            ready(document, function(){
+            ready(document, function() {
                 doInit(list, {}, callback);
             });
         },
 
-        sync: function(list, callback){
+        sync: function(list, callback) {
 
-            ready(document, function(){
+            ready(document, function() {
                 doInit(list, {
                     sync: true
                 }, callback);
